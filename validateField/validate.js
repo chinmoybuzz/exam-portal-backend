651cce6e4a86c98436cdb7182bdcbe5dc88cac1d
chinmoybuzz <chinmoybuzz@gmail.com>
2025-10-20 11:57:19 +0530

    user role add

--- validateField/validate.js
+++ validateField/validate.js
@@ -1,25 +1,20 @@
 const { body, validationResult } = require("express-validator");
 const { createResponse } = require("../utils/response");
 
-
 exports.validateSignup = [
-  body("name")
-    .notEmpty().withMessage("Name is required")
-    .isLength({ min: 2 }).withMessage("Name must be at least 2 characters"),
+  body("name").notEmpty().withMessage("Name is required").isLength({ min: 2 }).withMessage("Name must be at least 2 characters"),
 
-  body("email")
-    .isEmail().withMessage("Invalid email address"),
+  body("email").isEmail().withMessage("Invalid email address"),
 
-  body("password")
-    .isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
 
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const formattedErrors = {};
-      errors.array().forEach(err => {
+      errors.array().forEach((err) => {
         if (!formattedErrors[err.path]) {
-          formattedErrors[err.path] = err.msg
+          formattedErrors[err.path] = err.msg;
         }
       });
 
@@ -33,23 +28,20 @@ exports.validateSignup = [
       );
     }
     next();
-  }
+  },
 ];
 
 exports.validateLogin = [
-  body("email")
-    .notEmpty().withMessage("Email is required")
-    .isEmail().withMessage("Please enter a valid email"),
+  body("email").notEmpty().withMessage("Email is required").isEmail().withMessage("Please enter a valid email"),
 
-  body("password")
-    .notEmpty().withMessage("Password is required"),
+  body("password").notEmpty().withMessage("Password is required"),
 
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const formattedErrors = {};
-      errors.array().forEach(err => {
-        console.log(err)
+      errors.array().forEach((err) => {
+        console.log(err);
         if (!formattedErrors["error"]) {
           formattedErrors["error"] = "Please enter valid credentials";
         }
@@ -65,5 +57,31 @@ exports.validateLogin = [
       );
     }
     next();
-  }
+  },
+];
+exports.validateRole = [
+  body("role").notEmpty().withMessage("role is required").isEmail().withMessage("Please enter a valid role"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const formattedErrors = {};
+      errors.array().forEach((err) => {
+        console.log(err);
+        if (!formattedErrors["error"]) {
+          formattedErrors["error"] = "Please enter valid credentials";
+        }
+      });
+
+      return res.status(400).json(
+        createResponse({
+          status: 400,
+          success: false,
+          message: "Validation error",
+          data: formattedErrors,
+        })
+      );
+    }
+    next();
+  },
 ];
